d087c0dde24026d6ad57157ccc602da1d50ccafc
Jacob Larson <jacob.larson55112@gmail.com>
2021-11-18 10:26:03 -0600

    moved images to folder, cleaned up code

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+//Project inspired by the bleow kata from codeWars
 //https://www.codewars.com/kata/530e15517bc88ac656000716/train/javascript
-let sentence = 'Hello World!'
 
 function rot(message, number) {
   //let rotSentence = document.getElementById("rotInput").value;
  // let rotNumber = document.getElementById("rotNumber").value;
-    
+
+  //array to contain acsii values of each character 
   let coded = [];
-  //converter
+  //converter: pushes acsii values to "coded", which will be used later to add "number", and create a string with the new letters.
   for(let index in message) {
     //create ascii array
     coded.push(message.charCodeAt(index))
@@ -15,28 +16,30 @@ function rot(message, number) {
     let converted = '';
     
     for(let ascii of coded) {
-        //filters characters outside alphabet
+        //filters characters outside alphabet according to ascii values
       if(ascii < 65 || (ascii > 90 && ascii < 97) || ascii > 122) {
-        converted = converted.concat(String.fromCharCode(ascii))
+        converted += String.fromCharCode(ascii)
       }
-      //if uppercase
+      //if uppercase according to ascii values
       if(ascii >= 65 && ascii <= 90){
-          //add cipher
+          //once the values 
           ascii+=parseInt(number);
           //if in range add to return string
-          ascii >= 65 && ascii <= 90 ? converted = converted.concat(String.fromCharCode(ascii)) 
+          converted += ascii >= 65 && ascii <= 90
+              ? String.fromCharCode(ascii)
           //otherwise put it into range
-                    : converted = converted.concat(String.fromCharCode(ascii - 90 + 64))
+              : String.fromCharCode(ascii - 90 + 64)
       }
       //if lowercase
+      //same as above only using proper ascii values
       if(ascii >= 97 && ascii <= 122){
         ascii+=parseInt(number);
-        ascii >= 97 && ascii <= 122 ? converted = converted.concat(String.fromCharCode(ascii)) 
-                  : converted = converted.concat(String.fromCharCode(ascii - 122 + 96))
+        converted += ascii >= 97 && ascii <= 122 
+            ? String.fromCharCode(ascii)
+            : String.fromCharCode(ascii - 122 + 96)
       }
     }
   return document.getElementById("rot").innerHTML = `${converted}`
-  //if greater subtract and assign ot begining
 }
 //document.getElementById("rot").innerHTML = `${rot(sentence, 13)}`;
 //document.getElementById("rot").innerHTML = "asscheeks"
