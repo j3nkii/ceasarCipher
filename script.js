569ef426796e62bdc01b7a4038242a4185c2be37
Jacob Larson <jacob.larson55112@gmail.com>
2021-11-18 10:39:22 -0600

    added notes

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
 //Project inspired by the bleow kata from codeWars
 //https://www.codewars.com/kata/530e15517bc88ac656000716/train/javascript
 
+ //converter: pushes acsii values to "coded", which will be used later to add "number", and create a string with the new letters.
 function rot(message, number) {
-  //let rotSentence = document.getElementById("rotInput").value;
- // let rotNumber = document.getElementById("rotNumber").value;
 
   //array to contain acsii values of each character 
   let coded = [];
-  //converter: pushes acsii values to "coded", which will be used later to add "number", and create a string with the new letters.
+ 
+  //iterate through message and give it a numberical value to add the "number" later
   for(let index in message) {
-    //create ascii array
-    coded.push(message.charCodeAt(index))
+    coded.push(message.charCodeAt(index));
   }
-    //return string
-    let converted = '';
-    
-    for(let ascii of coded) {
-        //filters characters outside alphabet according to ascii values
-      if(ascii < 65 || (ascii > 90 && ascii < 97) || ascii > 122) {
-        converted += String.fromCharCode(ascii)
-      }
-      //if uppercase according to ascii values
-      if(ascii >= 65 && ascii <= 90){
-          //once the values 
-          ascii+=parseInt(number);
-          //if in range add to return string
-          converted += ascii >= 65 && ascii <= 90
-              ? String.fromCharCode(ascii)
-          //otherwise put it into range
-              : String.fromCharCode(ascii - 90 + 64)
-      }
-      //if lowercase
-      //same as above only using proper ascii values
-      if(ascii >= 97 && ascii <= 122){
+  //return string
+  let converted = '';
+  
+  for(let ascii of coded) {
+      //filters characters outside alphabet according to ascii values(uppercase=65-90)(lowercase=97-122)
+    if(ascii < 65 || (ascii > 90 && ascii < 97) || ascii > 122) {
+      converted += String.fromCharCode(ascii);
+    }
+    //if uppercase according to ascii values
+    if(ascii >= 65 && ascii <= 90){
+        //add the "number" to to the ascii value to be encrypted
         ascii+=parseInt(number);
-        converted += ascii >= 97 && ascii <= 122 
+        //if in range of the uppercase values, add to "converted" encryption
+        converted += ascii >= 65 && ascii <= 90
             ? String.fromCharCode(ascii)
-            : String.fromCharCode(ascii - 122 + 96)
-      }
+            //otherwise recalculate to start from begining of alphabet
+            : String.fromCharCode(ascii - 90 + 64);
+    }
+    //if lowercase
+    //same as above only using proper ascii values for the lowercase range 
+    if(ascii >= 97 && ascii <= 122){
+      ascii+=parseInt(number);
+      converted += ascii >= 97 && ascii <= 122 
+          ? String.fromCharCode(ascii)
+          : String.fromCharCode(ascii - 122 + 96);
+    }
     }
   return document.getElementById("rot").innerHTML = `${converted}`
 }
-//document.getElementById("rot").innerHTML = `${rot(sentence, 13)}`;
-//document.getElementById("rot").innerHTML = "asscheeks"
-//console.log(rot(rotSentence, rotNumber));
