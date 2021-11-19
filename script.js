9a719662bf8fc33b214dd26a9768f3e73514c452
Jacob Larson <jacob.larson55112@gmail.com>
2021-11-18 18:50:01 -0600

    i think i made it worse but it kind of takes negative values

--- script.js
+++ script.js
@@ -6,7 +6,6 @@
 //ex. a+1=b ; a+2=c.
 //instead of going through creating numberical values for each letter,
 //i used the  ascii values(https://www.w3schools.com/charsets/ref_html_ascii.asp).
-//Although, it will only function properly with 1-24 for the number input.
 //It also returns any character outside the Latin Alphabet
 
  //converter: pushes acsii values to "coded", which will be used later to add "number", and create a string with the new letters.
@@ -14,39 +13,61 @@ function rot(message, number) {
 
   //array to contain acsii values of each character 
   let coded = [];
- 
+  //makes numbers above 25 usable
+  number = number%26;
+  if(number < 0){
+    number = 25 - Math.abs(number)
+  }
   //iterate through message and give it a numberical value to add the "number" later
   for(let index in message) {
     coded.push(message.charCodeAt(index));
   }
-  if((number) > 24 || number < 1) {
-    return document.getElementById("rot").innerHTML = "SYSTEM OVERLOAD"
-  }
+  //if number will not work in function, return system overload and end funciton
+  //if(number < 0) {
+  //return document.getElementById("rot").innerHTML = "SYSTEM OVERLOAD"
+  //}
   //return string
   let converted = '';
   
   for(let ascii of coded) {
-      //filters characters outside alphabet according to ascii values(uppercase=65-90)(lowercase=97-122)
-    if(ascii < 65 || (ascii > 90 && ascii < 97) || ascii > 122) {
-      converted += String.fromCharCode(ascii);
-    }
-    //if uppercase according to ascii values
     if(ascii >= 65 && ascii <= 90){
-        //add the "number" to to the ascii value to be encrypted
-        ascii+=parseInt(number);
-        //if in range of the uppercase values, add to "converted" encryption
-        converted += ascii >= 65 && ascii <= 90
-            ? String.fromCharCode(ascii)
-            //otherwise recalculate to start from begining of alphabet
-            : String.fromCharCode(ascii - 90 + 64);
-    }
-    //if lowercase ---- same as above only using proper ascii values for the lowercase range 
-    if(ascii >= 97 && ascii <= 122){
+      //add the "number" to to the ascii value to be encrypted
       ascii+=parseInt(number);
-      converted += ascii >= 97 && ascii <= 122 
+      //if in range of the uppercase values, add to "converted" encryption
+      converted += ascii >= 65 && ascii <= 90
           ? String.fromCharCode(ascii)
-          : String.fromCharCode(ascii - 122 + 96);
+          //otherwise recalculate to start from begining of alphabet
+          : String.fromCharCode(ascii - 26) 
+  } else if(ascii >= 97 && ascii <= 122){
+        //cool, now add the roation
+        ascii+=parseInt(number);
+        //is it still a letter?
+        converted += ascii >= 97 && ascii <= 122 
+        //yes? cool, return that later
+            ? String.fromCharCode(ascii)
+            //no? well, was it greater than 0?
+            : String.fromCharCode(ascii - 26) 
     }
-    }
+  }
   return document.getElementById("rot").innerHTML = `${converted}`
+}
+
+
+
+
+
+
+/*TESTCASE
+let fuckmerunning = 0;
+while(fuckmerunning <= 26){
+  console.log(fuckmerunning ,rot('ABCDEFGHIJKLMNOPQRSTUVWXYZ', fuckmerunning));
+  console.log(fuckmerunning ,rot('abcdefghijklmnopqrstuvwxyz', fuckmerunning));
+  fuckmerunning++
+}
+
+fuckmerunning = 0;
+while(fuckmerunning >= -26){
+  console.log(fuckmerunning ,rot('ABCDEFGHIJKLMNOPQRSTUVWXYZ', fuckmerunning));
+  console.log(fuckmerunning ,rot('abcdefghijklmnopqrstuvwxyz', fuckmerunning));
+  fuckmerunning--
 }
